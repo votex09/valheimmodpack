e40f582c61b9eb8e7d0754ce39be68bc8f004fa5
Kyler Juresic <kylerjuresic@gmail.com>
2021-12-17 15:49:14 -0600

    Arrays do not like to be quoted

--- PackBin/getfromgit.ps1
+++ PackBin/getfromgit.ps1
@@ -234,11 +234,11 @@ if ($args[0] -eq "-cfglock") {
             Write-Host "=============================`nConfig Lock Menu`n============================="
             Write-Host "This menu locks certain configs from changing on update.`nThis is the only way for keybindings for mods to persist between updates.`nEnter [X] to exit`n`n" -ForegroundColor Blue
             Write-Host "Current Locked Configs:"
-            #retrieve list of locked configs
+            #retrieve list of locked configs from file
             $locked = Get-Content -Path "$PSScriptRoot\PackBin\git\valheimdirtbagmodpack\.git\info\exclude"
             #loop through list and print to screen
             for ($i = 0; $i -lt $locked.Count; $i++) {
-                Write-Host "`n$locked[$i]" -ForegroundColor Yellow
+                Write-Host $locked[$i] -ForegroundColor Yellow
             }
             Write-Host "`n------------------------------`n"
             for ($i = 0; $i -lt $availablelocks.Count; $i++) {
