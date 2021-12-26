c1c2830f6952a4da209e14c95ed453988119afd8
Kyler Juresic <kylerjuresic@gmail.com>
2021-12-26 01:34:15 -0600

    Shall I finally do it right this time?

--- PackBin/getfromgit.ps1
+++ PackBin/getfromgit.ps1
@@ -546,8 +546,8 @@ if ($args[0] -eq "-update")
             #download mods that are missing or are out of date from ModVer based on the pack.ini file
             foreach ($mod in $config.GetEnumerator())
             {
-                Write-Host "$($mod.name) - Remote: v$($mod.value.version) Local: v$($localModList.BepInEx)" -ForegroundColor Yellow
-                if (!($localModList."$($mod.name)") -or ($localModList."$($mod.name)".version -ne $mod.value.version))
+                #Write-Host "$($mod.name) - Remote: v$($mod.value.version) Local: v$($localModList.BepInEx)" -ForegroundColor Yellow
+                if (!($localModList."$($mod.name)") -or ($localModList.$($mod.name) -ne $mod.value.version))
                 {
                     if ($config."$($mod.name)".specialinstall = "false")
                     {
